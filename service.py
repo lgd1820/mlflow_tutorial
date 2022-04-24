6fce02eb793b7269034aa63837f98fdb4f53db46
lgd1820 <lgd1810@gmail.com>
2022-04-24 13:33:51 +0900

    update

--- service.py
+++
@@ -1,9 +0,0 @@
-from bentoml import artifacts, api, BentoService
-from bentoml.frameworks.pytorch import PytorchModelArtifact
-from bentoml.adapters import JsonInput
-
-@artifacts([PytorchModelArtifact('model')])
-class MnistClassifier(BentoService):
-    @api(input=JsonInput()):
-    def predict(self, parsed_jsons):
-        
